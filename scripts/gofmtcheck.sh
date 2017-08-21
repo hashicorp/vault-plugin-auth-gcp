522c4d62f04ffbbdc141061d3673501a0ca94a87
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2017-08-21 10:25:03 -0400

    Add gofmtcheck

--- scripts/gofmtcheck.sh
+++ scripts/gofmtcheck.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 echo "==> Checking that code complies with gofmt requirements..."
 
 gofmt_files=$(gofmt -l `find . -name '*.go' | grep -v vendor`)
-if [[ -n ${gofmt_files} ]]; then
+if [[ -n ${gofmt_files} ]]; then 
     echo 'gofmt needs running on the following files:'
     echo "${gofmt_files}"
     echo "You can use the command: \`make fmt\` to reformat code."
