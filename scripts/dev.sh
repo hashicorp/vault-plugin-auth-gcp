fea9c80837edd121d85a015b467a34d6bd5c9609
emily <emilyye@google.com>
2019-12-15 22:30:20 -0800

    Get service account via public URL, fix GCE inferrence bug (#77)

---
+++ scripts/dev.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -eEuo pipefail
+
+MNT_PATH="gcp"
+PLUGIN_NAME="vault-plugin-auth-gcp"
+
+#
+# Helper script for local development. Automatically builds and registers the
+# plugin. Requires `vault` is installed and available on $PATH.
+#
+
+# Get the right dir
+DIR="$(cd "$(dirname "$(readlink "$0")")" && pwd)"
+
+echo "==> Starting dev"
+
+echo "--> Scratch dir"
+echo "    Creating"
+SCRATCH="${DIR}/tmp"
+mkdir -p "${SCRATCH}/plugins"
+
+function cleanup {
+  echo ""
+  echo "==> Cleaning up"
+  kill -INT "${VAULT_PID}"
+  rm -rf "${SCRATCH}"
+}
+trap cleanup EXIT
+
+echo "--> Building"
+go build -o "${SCRATCH}/plugins/${PLUGIN_NAME}"
+
+echo "--> Starting server"
+
+export VAULT_TOKEN="root"
+export VAULT_ADDR="http://127.0.0.1:8200"
+
+vault server \
+  -dev \
+  -dev-plugin-init \
+  -dev-plugin-dir "${SCRATCH}/plugins" \
+  -dev-root-token-id "root" \
+  -log-level "debug" \
+  &
+sleep 2
+VAULT_PID=$!
+
+echo "    Mounting plugin"
+vault auth enable -path=${MNT_PATH} -plugin-name=${PLUGIN_NAME} plugin
+
+echo "==> Ready!"
+wait ${VAULT_PID}
